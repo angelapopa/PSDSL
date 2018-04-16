e3ece926b594fd5a0a70bd272c581456d9933c96
angelapopa <popa.angela@gmail.com>
2018-04-16 22:47:26 +0200

    attempt to fix grammar, with no success

--- Assignment4/amazon.groovy
+++ Assignment4/amazon.groovy
@@ -1,10 +1,13 @@
-def instance1 = new EC2Instance()
-
-def ram(int nrOfRamUnits){
-//this_is_just_a_stub
+def cpu(int units){
+	//this is just a method stub
 }
 
-def instance2 = new EC2Instance().ram(1024)
+def ram(int units){
+	//this is just a method stub
+}
+
+def instance1 = new EC2Instance().cpu(2)
+def instance2 = new EC2Instance().ram(1024).cpu(4)
 
 instance1.run()
 instance2.run()
