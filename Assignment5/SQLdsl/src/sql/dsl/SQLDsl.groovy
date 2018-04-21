de55ca2f35b357af3ad9d9b3194e30728783793b
angelapopa <popa.angela@gmail.com>
2018-04-21 21:23:52 +0200

    add select with simple where clause

--- Assignment5/SQLdsl/src/sql/dsl/SQLDsl.groovy
+++ Assignment5/SQLdsl/src/sql/dsl/SQLDsl.groovy
@@ -1,45 +1,65 @@
-package sql.dsl;
-
+package sql.dsl
 import java.nio.channels.SelectableChannel;
 
-class SQLStatement{
+class SQLStatement3{
 
-	def ALL = '*';
+	static String ALL = '*';
+	static String SELECT = 'SELECT'
+	static String FROM = 'FROM'
+	static String WHERE = 'WHERE'
 	
-	def SELECT = 'SELECT';
+	static operations = ['is greater than' : '>', 'is smaller than': '<', 'equals': '=']
+}
+
+class SelectStatement3 extends SQLStatement3 {
 	
+	def field
 	
-	def SQLDsl(value){
-		return value;
+	def table
+	
+	def expression
+
+	def printStatement() {
+		def newExpression = replace(expression)
+		println "$SELECT $field $FROM $table $WHERE $newExpression"
 	}
 	
-	def select(String value){
-		return value == null ? new SelectStatement(ALL) : new SelectStatement(value);
+	def replace(String userExpression) {
+		operations.each { key, operation ->
+			if (userExpression.contains(key)){
+				userExpression = userExpression.replace(key, operation)
+				if (operation == '=') {  // handle int and strings differently
+					String[] parts = userExpression.split(operation)
+					userExpression = parts[0].concat(operation).concat(" ").concat(convertToString(parts[1]))
+				}
+			}
+		}
+		return userExpression;
 	}
+	
+	def convertToString(String stringToConvert){
+		try{
+			return Integer.parseInt(stringToConvert.trim()).toString()
+		}catch (NumberFormatException ex){
+			return "'".concat(stringToConvert.trim()).concat("'")
+		}
+	}
+}
+
+def select(name) {
+	name == 'all records' ? name = SQLStatement3.ALL : name
+	[from : {
+		table ->
+		[ where: { condition ->
+			new SelectStatement3(field: name, table: table, expression: condition).printStatement();
+		}]
+	}]
 	
 }
 
-class SelectStatement{
-	
-	def selectSt;
-	
-	def table;
-	
-	def SelectStatement(String value){
-		this.selectSt = value;
-	}
-	
-	def from(table){
-		this.table = table;
-		return this
-	}
-	
-	def printSelectStatement() {println "Select $selectSt from $table"}
-}
+select 'all records' from "MyTable" where "name is greater than 1"
+select 'name' from 'newTable' where 'name equals John'
+//select 'name' from 'newTable' where 'name = John' and 'firstname = Smith'
 
-
-def result = new SQLStatement().select().from('MyTable')
-result.printSelectStatement()
-
-//def result2 = new SQLStatement().select from 'MyTable'
-
+select 'name' from 'newTable' where 'name = John and firstname = Smith'
+select 'name' from 'newTable' where 'name = John or firstname = Smith'
--- Assignment5/SQLdsl/src/sql/dsl/SQLdslnew.java
+++
@@ -1,5 +0,0 @@
-package sql.dsl;
-
-class SQLdslnew {
-
-}
