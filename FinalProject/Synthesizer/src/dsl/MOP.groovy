040e8fbf73ca8bac418a65fbd7d66a84e9914304
ngapham <ngapt.fit@gmail.com>
2018-06-05 11:14:49 +0200

    Cleanup code

--- FinalProject/Synthesizer/src/dsl/MOP.groovy
+++ FinalProject/Synthesizer/src/dsl/MOP.groovy
@@ -1,6 +1,7 @@
-package tryout
+package dsl
 
 import java.applet.Applet
+import java.awt.BorderLayout
 import java.nio.file.attribute.AclEntry.Builder;
 
 import org.apache.ivy.core.module.descriptor.ExtendsDescriptor;
@@ -8,84 +9,100 @@ import org.apache.ivy.core.module.descriptor.ExtendsDescriptor;
 import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
 import com.jsyn.unitgen.LineOut;
+import com.jsyn.unitgen.LinearRamp;
 import com.jsyn.unitgen.SineOscillator
+import com.jsyn.unitgen.UnitOscillator
+import com.jsyn.swing.ExponentialRangeModel;
+import com.jsyn.swing.JAppletFrame;
+import com.jsyn.swing.PortControllerFactory;
+import com.jsyn.swing.PortModelFactory;
+import com.jsyn.swing.RotaryTextController;
 
 import groovy.json.JsonSlurper
 import groovy.text.SimpleTemplateEngine
 import groovy.ui.ConsoleApplet
 
+// Database
 def filePath = new File(".").absoluteFile.getParent()
 def sluper = new JsonSlurper()
 def lineOuts = sluper.parse(new FileReader(filePath + '/src/json/lineOuts.json'))
-def oscillators = sluper.parse(new FileReader(filePath + '/src/json/oscillators.json'))
+def sounds = sluper.parse(new FileReader(filePath + '/src/json/sounds.json'))
 
-Synthesizer.metaClass.addOsci << {list ->
-	int count = 0
-	list.each {
-		// handle Unit name
-		/*
-		String script = '''<% print "$name"%>'''
-		def binding = [
-			name: it.name
-		]
-		def engine = new SimpleTemplateEngine()
-		def template = engine.createTemplate(script).make(binding)
-		String name = template.toString()
-		*/
-		if (it.type == 'SineOscillator') {
-			SineOscillator myOsc
-			add(myOsc = new SineOscillator())
-		}
-		count++
-		println "Added new $it.type "//$name"
-	}
-}
-
-Synthesizer.metaClass.addLineOut << {list ->
-	list.each {
-		LineOut myLineOut
-		add(myLineOut = new LineOut())
-		println "Added new LineOut"
-	}
-}
-
+UnitOscillator myOsc
+LineOut myLineOut
 Synthesizer s
+LinearRamp lag
+
+// Meta programming
+Synthesizer.metaClass.addUnits << {listOsci, listLineOut ->
+	int count = 0
+	listOsci.each {
+		if (it.type == 'SineOscillator') {
+			myOsc = new SineOscillator()
+			myOsc.frequency.setup(it.frequency.minimum, it.frequency.defaultValue, it.frequency.maximum)
+			add(myOsc)
+			println "Added new $it.type "//$name"
+			println "with frequency($it.frequency.minimum, $it.frequency.defaultValue, $it.frequency.maximum)"
+			listLineOut.each {
+				myLineOut = new LineOut()
+				add(myLineOut)
+				myOsc.output.connect(0, myLineOut.input, 0)
+				myOsc.output.connect(0, myLineOut.input, 1)
+			}
+		}
+	}
+}
+
+/*
+UnitOscillator.metaClass.connectToLineOut << {lineOut, leftChannel, rightChannel = false ->
+	if (leftChannel) {
+		output.connect(0, lineOut.input, 0)
+	}
+	if (rightChannel) {
+		output.connect(0, lineOut.input, 1)
+	}
+}
+*/
+
+// Define block functions
 def startSynthesisEngine() {
 	s = new JSyn().createSynthesizer()
 	s.start()
-}
-
-def buildUnitGenerators(def listOsci, def listLineOut) {
-	s.addOsci(listOsci)
-	s.addLineOut(listLineOut)
-}
-
-def connectUnitGenerators() {
 	
+	lag = new LinearRamp()
+	lag.input.setup(0, 0.5, 1.0)
+	s.add(lag)
 }
 
-def startUnitGenerators() {
-	
+def buildAndConnectUnits(def listOsci, def listLineOut) {
+	s.addUnits(listOsci, listLineOut)
 }
 
-startSynthesisEngine()
-buildUnitGenerators(oscillators, lineOuts)
 
+/*
+ * Start main() function
+ */
+// Just release the block function. In the future, we can use block funtion instead
+s = new JSyn().createSynthesizer()
+s.start()
+
+lag = new LinearRamp()
+lag.input.setup(0, 0.5, 1.0)
+s.add(lag)
+s.addUnits(sounds, lineOuts)
+
+// Start UIs
 def builder = new groovy.swing.SwingBuilder()
 def frame = builder.frame(
-	title: 'Swing',
-	size: [500, 300],
-//	layout: new java.awt.FlowLayout(),
-	defaultCloseOperation: javax.swing.WindowConstants.EXIT_ON_CLOSE,
-	show: true
-	
-	) {
-	
-	}
-/*
-public class AppletFrame extends Applet {
-	public void start() {
+		title: 'Swing',
+		size: [500, 300],
+		defaultCloseOperation: javax.swing.WindowConstants.EXIT_ON_CLOSE,
+		show: true
+
+		) {
+			gridLayout(cols: 1, rows: 2)
+			def amplitudeModel = PortModelFactory.createExponentialModel(lag.input)
+			panel(new RotaryTextController(amplitudeModel, 5))
+			slider(PortControllerFactory.createExponentialPortSlider(myOsc.frequency))
+		}
 		
-		
-	}
-}*/
--- FinalProject/Synthesizer/src/dsl/TransformToSynthesizer.groovy
+++ FinalProject/Synthesizer/src/dsl/TransformToSynthesizer.groovy
@@ -6,29 +6,26 @@ def dsl = new File(filePath + '/src/dsl/synthesizerEditor.dsl').text
 def controlsOutput = filePath + 'src/json/controls.json'
 def soundsOutput = filePath + 'src/json/sounds.json'
 def componentsOutput = filePath + 'src/json/components.json'
-
+def lineOutsOutput = filePath + 'src/json/lineOuts.json'
+/*
 def layoutsOutput = filePath + 'src/json/layouts.json'
 def linearRampsOutput = filePath + 'src/json/linearRamps.json'
-def lineOutsOutput = filePath + 'src/json/lineOuts.json'
 def gridLayoutsOutput = filePath + 'src/json/gridLayouts.json'
 def framesOutput = filePath + 'src/json/frames.json'
-
+*/
 def script = """
 ${dslDef}
 
 ${dsl}
 
-println controls.get(0).getX()
-// ERROR here; delete file will handle exception
-
 new File('${controlsOutput}').write(new groovy.json.JsonBuilder(controls).toPrettyString())
 new File('${soundsOutput}').write(new groovy.json.JsonBuilder(sounds).toPrettyString())
 new File('${componentsOutput}').write(new groovy.json.JsonBuilder(components).toPrettyString())
-new File('${linearRampsOutput}').write(new groovy.json.JsonBuilder(linearRamps).toPrettyString())
+new File('${lineOutsOutput}').write(new groovy.json.JsonBuilder(lineOuts).toPrettyString())
 """
 /*
+new File('${linearRampsOutput}').write(new groovy.json.JsonBuilder(linearRamps).toPrettyString())
 new File('${layoutsOutput}').write(new groovy.json.JsonBuilder(layouts).toPrettyString())
-new File('${lineOutsOutput}').write(new groovy.json.JsonBuilder(lineOuts).toPrettyString())
 new File('${gridLayoutsOutput}').write(new groovy.json.JsonBuilder(gridLayouts).toPrettyString())
 new File('${framesOutput}').write(new groovy.json.JsonBuilder(frames).toPrettyString())
 */
